c0a7b88d4e102fae875fae3471b919ab14033a3c
Daniellamaral <160861545+Daniellamaral@users.noreply.github.com>
2024-03-28 07:35:21 -0300

    Update seu_nome_questao_02.py
    Repetição de nome

--- seu_nome_questao_02.py
+++ seu_nome_questao_02.py
@@ -1,3 +1,9 @@
 #  Questão 02 [1,0 pt (entrega) + 1,0 pt (oral)]
 
 # Faça um programa que imprima o seu nome 1 milhão de vezes na tela
+
+# Programa para imprimir seu nome 1 milhão de vezes na tela
+nome = "Daniella"
+
+for _ in range(1000000):
+    print(Daiella)
